cda7ae2aee1644ba27ea3c535dcb51b18ac02cde
pat2903 <103569848+pat2903@users.noreply.github.com>
2024-02-03 11:59:53 +0000

    😭

--- adahackwebsite/src/App.js
+++ adahackwebsite/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  async function handleClick(){
+    const res = await fetch('https://api.smartsheet.com/2.0/sheets/5154267455246212', {
+      method: 'GET',
+      headers: {
+        'Authorization':'Bearer 8j4MNyQBnOuB0WWMhUM5dnm9sixNDWCVMhSuP',
+        'Content-Type':'application/json',
+        'mode':'no-cors'
+        // 'Access-Control-Allow-Origin':'*',
+        // 'Access-Control-Allow-Methods':'GET, POST,PATCH,OPTIONS'
+      },
+    })
+    const data = await res.json()
+    console.log(data)
+  }
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick}>BUY</button>
       </header>
+      <div>
+
+      </div>
     </div>
   );
 }
